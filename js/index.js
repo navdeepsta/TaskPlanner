73c7c3ee7b7c0f0717fb7ea7ffd35c9ab8ff9a2e
navdeep <navdeepst.1987@gmail.com>
2021-09-23 13:22:50 +1000

    updating form validation 1

--- js/index.js
+++ js/index.js
@@ -1,83 +1,118 @@
 /*  Selecting elements from the index.html  */
-const taskSubmit = document.querySelector('#task-submit');
-const taskReset = document.querySelector('#task-reset');
-const taskName = document.querySelector('#task-name');
-const taskDescription = document.querySelector('#task-description');
-const taskAssign = document.querySelector('#task-assign');
-const taskDate = document.querySelector('#task-date');
-const taskStatus = document.querySelector('#task-status');
-const spanError =  document.getElementsByClassName('err-task');
+const taskSubmit = document.querySelector("#task-submit");
+const taskReset = document.querySelector("#task-reset");
+const taskName = document.querySelector("#task-name");
+const taskDescription = document.querySelector("#task-description");
+const taskAssign = document.querySelector("#task-assign");
+const taskDate = document.querySelector("#task-date");
+const taskStatus = document.querySelector("#task-status");
+const spanError = document.getElementsByClassName("err-task");
 const errors = [true, true, true, true, true];
 
 /* Declaring variables to store form field values */
-let nameData, descriptionData, taskAssignData, 
-    taskDateData, taskStatusData, errorMessage;
+let nameData,
+    descriptionData,
+    taskAssignData,
+    taskDateData,
+    taskStatusData,
+    errorMessage;
 
 /* Applying event listeners on input fields */
-taskName.addEventListener('focusout', ()=>{
+taskName.addEventListener("focusout", () => {
     nameData = taskName.value;
-    errorMessageGenerator(taskName, spanError[0], nameData);  
+    errorMessage = " Enter alteast 5 letters";
+    errorMessageGenerator(
+        taskName,
+        spanError[0],
+        nameData,
+        errorMessage
+    );
     updateSubmission();
 });
 
-taskDescription.addEventListener('focusout', ()=>{
+taskDescription.addEventListener("focusout", () => {
     descriptionData = taskDescription.value;
-    errorMessageGenerator(taskDescription, spanError[1], descriptionData);
+    errorMessage = " Enter alteast 5 letters";
+    errorMessageGenerator(
+        taskDescription,
+        spanError[1],
+        descriptionData,
+        errorMessage
+    );
     updateSubmission();
 });
 
-taskAssign.addEventListener('focusout', ()=>{
+taskAssign.addEventListener("focusout", () => {
     taskAssignData = taskAssign.value;
-    errorMessageGenerator(taskAssign, spanError[2], taskAssignData);
+    errorMessage = " Enter alteast 5 letters";
+    errorMessageGenerator(
+        taskAssign,
+        spanError[2],
+        taskAssignData,
+        errorMessage
+    );
     updateSubmission();
 });
 
-taskDate.addEventListener('focusout', ()=>{
-    taskDateData = taskDate.value;
-    if( !taskDateData ) {
-        errorMessageGenerator(taskDate, spanError[3], '');
-        errorMessage =' Enter the correct date';
-        spanError[3].innerHTML = errorMessage;
-    } else {
-        errorMessageStyleReset(taskDate, spanError[3]);
-        
+taskDate.onclick = ()=>{
+    let now = new Date();
+    let month = now.getMonth() + 1;
+    if(month < 10){
+        month = '0'+(month);
     }
-   
-});
-
-taskStatus.addEventListener('focusout', ()=>{
-    taskStatusData = taskStatus.value;
-    if( taskStatusData === 'Select...')
-    {
-        errorMessageGenerator(taskStatus, spanError[4], '');
-        errorMessage = ' Select a status';
-        spanError[4].innerHTML = errorMessage;
-    } else{
-        errorMessageStyleReset(taskStatus, spanError[4]);
-    }
-});
-
-function errorMessageGenerator( parentElement, spanElement, data ) {   
-    data.length < 5   ? errorMessageStyle(parentElement, spanElement) 
-                      : errorMessageStyleReset(parentElement, spanElement);
+    let currentDate = now.getFullYear()+"-"+month+"-"+now.getDate();
+    taskDate.setAttribute('min', currentDate);
 }
 
-function errorMessageStyle( parentElement, spanElement ) {
-    parentElement.style.border = '1px solid red';
-    errorMessage = ' Enter alteast 5 letters';
+taskDate.addEventListener("focusout", () => {
+    taskDateData = taskDate.value;
+    errorMessage = " Enter the correct date";
+    !taskDateData
+        ? errorMessageGenerator(
+            taskDate,
+            spanError[3],
+            "",
+            errorMessage
+        )
+        : errorMessageStyleReset(taskDate, spanError[3]);
+    updateSubmission();
+});
+
+taskStatus.addEventListener("focusout", () => {
+    taskStatusData = taskStatus.value;
+    errorMessage = " Select a status";
+    taskStatusData === "Select..."
+        ? errorMessageGenerator(taskStatus, spanError[4], "", errorMessage)
+        : errorMessageStyleReset(taskStatus, spanError[4]);
+    updateSubmission();
+});
+
+function errorMessageGenerator(parentElement, spanElement, data, errorMessage) {
+    data.length < 5
+        ? errorMessageStyle(parentElement, spanElement, errorMessage)
+        : errorMessageStyleReset(parentElement, spanElement);
+}
+
+function errorMessageStyle(parentElement, spanElement, errorMessage) {
+    parentElement.style.border = "1px solid red";
     spanElement.innerHTML = errorMessage;
-    spanElement.style.color = 'red';
+    spanElement.style.color = "red";
+    spanElement.style.backgroundColor = "#eee";
+    spanElement.style.marginLeft = "10px";
+    spanElement.style.padding = "5px";
+    spanElement.style.borderRadius = "5px";
     updateErrors(parentElement, true);
 }
 
-function errorMessageStyleReset( parentElement, spanElement ) {
-    spanElement.innerHTML = '';
-    parentElement.style.border = '';
-    updateErrors(parentElement, false);  
+function errorMessageStyleReset(parentElement, spanElement) {
+    spanElement.innerHTML = "";
+    parentElement.style.border = "";
+    spanElement.style.backgroundColor = "";
+    updateErrors(parentElement, false);
 }
 
-function updateErrors( parentElement, errorFlag ) {
-    switch( parentElement.id ) {
+function updateErrors(parentElement, errorFlag) {
+    switch (parentElement.id) {
         case taskName.id:
             errors[0] = errorFlag;
             break;
@@ -87,18 +122,28 @@ function updateErrors( parentElement, errorFlag ) {
         case taskAssign.id:
             errors[2] = errorFlag;
             break;
-    } 
+        case taskDate.id:
+            errors[3] = errorFlag;
+            break;
+        case taskStatus.id:
+            errors[4] = errorFlag;
+            break;
+    }
 }
 
 // enable or disable submit button based on the result of validation
 function updateSubmission() {
-    if(errors[0] || errors[1] || errors[2]) {
+    if (errors[0] || errors[1] || errors[2] || errors[3] || errors[4]) {
         taskSubmit.disabled = true;
-    }else{
+    } else {
         taskSubmit.disabled = false;
     }
 }
 
-taskSubmit.addEventListener('click', validFormFieldInput);
+function validFormFieldInput() {
+   
+ }
 
-const taskManager = new TaskManager();
+taskSubmit.addEventListener("click", validFormFieldInput);
+
+
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,9 +1,6 @@
 class TaskManager {
-    constructor() {
-        this._tasks = [1,2,3,4];
-    }
-
-    get tasks() {
-        return  this._tasks;
+    constructor(currentId = 0) {
+        this._tasks = [];
+        this._currentId = currentId;
     }
 }
