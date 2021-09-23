99fdd632be8b3fbf96739e2771b0c39070fb9497
Sara Amir <saraamir@192-168-1-107.tpgi.com.au>
2021-09-23 14:14:57 +1000

    adding a task in array

--- js/taskManager.js
+++ js/taskManager.js
@@ -3,4 +3,19 @@ class TaskManager {
         this._tasks = [];
         this._currentId = currentId;
     }
+    get tasks() {
+        return this._tasks;
+    }
+
+    addTask(taskName, taskDescription, taskAssign, taskDate, taskStatus) {
+        const task = {
+            taskId:  ++this._currentId,
+            name: taskName,
+            description: taskDescription, 
+            assign: taskAssign, 
+            date: taskDate, 
+            status: taskStatus
+        }
+        this._tasks.push(task);
+    }
 }
